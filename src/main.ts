e9a9749e99fb9f3fe5448ebc7cc03e0640d09ba0
addisonsouza <addison.souza@dixiponto.com.br>
2026-02-12 00:04:56 -0300

    feat: implementing docker configurarion

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { CorsConfigService } from './config/cors-config.service';
-import 'source-map-support/register';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
